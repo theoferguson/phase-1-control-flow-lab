8ecb375b1fbcf50f0e45c8cce3d7da2575ff453d
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-09 13:06:45 -0400

    commit tree e7377751cb3bf2a37031207b8a92dc231f818416

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance < 400) {
+    return 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+  return (destinationCity === "NYC" ? 'Ok, sounds good.' : 'No go.');  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
